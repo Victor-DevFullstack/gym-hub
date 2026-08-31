a1dcab8264262462ff94626b2e684b6583fb463a
Victor André Lima Mourão de Oliveira <elane.asfunix99@email.com>
2026-08-31 16:55:16 -0300

    feat(criação dos inputs de login)

--- src/app/features/login/login/login.ts
+++ src/app/features/login/login/login.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule,MatCheckboxModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
-})
-export class Login {}
+})  
+export class Login {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email, Validators.pattern("^[^\s@]+@[^\s@]+\.[^\s@]+$")]);
+
+  constructor() {
+    console.log(this.emailFormControl);
+    
+  }
+  // matcher = new
+}
