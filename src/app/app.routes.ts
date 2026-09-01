59606631c25b85e3dd79931976dd12bbb4ed06d2
Wellington Barbosa <huehueb58@gmail.com>
2026-09-01 14:12:33 -0300

    feat(componente de cadastro)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
   },
+  {
+    path: 'cadastro',
+    loadComponent: () => import('./features/auth/cadastro/cadastro').then((m) => m.Cadastro),
+  },
 ];
