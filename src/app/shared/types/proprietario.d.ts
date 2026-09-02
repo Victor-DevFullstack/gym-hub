68cc325c9ea2cbbc484e7cc72492fec760c09518
Wellington Barbosa <106507628+wellingtonbarbosadev@users.noreply.github.com>
2026-09-02 10:21:59 -0300

    feat(feito a validacao de login)

--- src/app/shared/types/proprietario.d.ts
+++ src/app/shared/types/proprietario.d.ts
@@ -1,15 +1,15 @@
 import { FormControl } from "@angular/forms"
 
 type ProprietarioType = {
-  nome: string,
-  nomeAcademia: string,
+  nome?: string,
+  nomeAcademia?: string,
   email: string,
   senha: string
 }
 
 type ProprietarioFormControls = {
-  nome: FormControl<string>
-  nomeAcademia: FormControl<string>
+  nome?: FormControl<string>
+  nomeAcademia?: FormControl<string>
   email: FormControl<string>
   senha: FormControl<string>
 }
