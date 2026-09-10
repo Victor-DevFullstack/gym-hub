dca26bb0f356a4d0e63153595c02eb1a62fd8fa0
Wellington Barbosa <huehueb58@gmail.com>
2026-09-10 10:22:28 -0300

    feat(Implementação do logout e persistência do usuário na autenticação)

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,22 +1,30 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { effect, inject, Injectable, signal } from '@angular/core';
 import { UsuarioService } from './usuario.service';
 import { UsuarioType } from '../../shared/types/usuario';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private usuarioService = inject(UsuarioService);
+  private router = inject(Router);
   private chave = 'usuarioLogado';
 
-  usuarioLogado = signal<UsuarioType | null>(null)
+  usuarioLogado = signal<UsuarioType | null>(JSON.parse(localStorage.getItem(this.chave) || 'null'));
+  
+  constructor() {
+    effect(() => {
+      const usuario = this.usuarioLogado();
+      localStorage.setItem(this.chave, JSON.stringify(usuario));
+    })
+  }
 
   login(email: string, senha: string): { user?: UsuarioType } {
     const { user } = this.usuarioService.login(email, senha);
 
     if (user) {
-      localStorage.setItem(this.chave, JSON.stringify(user));
-      this.usuarioLogado.set(user)
+      this.usuarioLogado.set(user);
       return { user };
     }
 
@@ -24,12 +32,12 @@ export class AuthService {
   }
 
   logout(): void {
-    localStorage.removeItem(this.chave);
+    this.usuarioLogado.set(null);
+    this.router.navigate(['/']);
   }
 
   getUsuarioLogado(): UsuarioType | null {
-    const dados = localStorage.getItem(this.chave);
-    return dados ? JSON.parse(dados) : null;
+    return this.usuarioLogado();
   }
 
   isLogado(): boolean {
--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,8 +1,8 @@
 import { Component, inject, signal } from '@angular/core';
-import { MatCardModule } from "@angular/material/card";
+import { MatCardModule } from '@angular/material/card';
 import { AuthService } from '../../../core/services/auth.service';
 import { TitleCasePipe } from '@angular/common';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,26 +10,20 @@ import { RouterLink } from "@angular/router";
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-
 export class Header {
-  private authService = inject(AuthService)
+  private authService = inject(AuthService);
 
-  user = this.authService.getUsuarioLogado()
+  user = this.authService.getUsuarioLogado();
 
   notificacoesAbertas = signal(false);
 
   dataAtual = new Date().toLocaleDateString('pt-BR');
 
   exibirNotificacoes() {
-    this.notificacoesAbertas.update(valor => !valor);
+    this.notificacoesAbertas.update((valor) => !valor);
   }
 
-  constructor() {
-    console.log(
-
-      this.user?.nome.slice(0,1)
-    );
-    
-
+  logout() {
+    this.authService.logout();
   }
 }
