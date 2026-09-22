94f43a018ad4d2f0ba0661e1aa1132039199e8e9
Victor André Lima Mourão de Oliveira <elane.asfunix99@email.com>
2026-09-22 16:42:23 -0300

    feat(Inicio do endereço)

--- src/app/shared/components/settings/endereco/endereco.ts
+++ src/app/shared/components/settings/endereco/endereco.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-endereco',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './endereco.html',
   styleUrl: './endereco.css',
 })
